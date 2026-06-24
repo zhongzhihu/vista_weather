75abee01e8b15c5c4972a240a03b2feb5541b59f
Zhongzhi Hu <zhongzhihu@hotmail.com>
2026-06-25 01:11:55 +0200

    netlify deploymetn

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zhongzhihu.github.io",
-  base: "/vista_weather",
+  site: "https://vistaweather.com",
   compressHTML: true,
 });
+
